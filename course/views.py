e7b365abff15853f7a6763c17283966ffbfc16a2
ArmaanChaand <armaan.chaand17@gmail.com>
2023-06-06 18:42:18 +0530

    Course

--- course/views.py
+++ course/views.py
@@ -4,8 +4,7 @@ from .models import Course
 from .serializers import CourseSerializer
 
 @api_view(http_method_names=['GET'])
-def getAllCourses(request):
-    response_data = {}        
+def getAllCourses(request):    
     courses = Course.objects.all()
-    response_data['courses'] = CourseSerializer(courses, many=True).data
-    return Response(response_data)
+    all_courses = CourseSerializer(courses, many=True).data
+    return Response(all_courses)
