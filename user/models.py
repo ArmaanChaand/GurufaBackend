a0f2f76a1faf9d25f70144a9824992dbc5cff24d
ArmaanChaand <armaan.chaand17@gmail.com>
2023-08-31 01:44:09 +0530

    Bugs 3

--- user/models.py
+++ user/models.py
@@ -115,7 +115,7 @@ class Kid(models.Model):
     kid_last_name     = models.CharField(_("Last name"), max_length=50, blank=False, null=False)
     kid_age           = models.PositiveIntegerField(_("Kid's Age (In Years)"), default=5, validators=[validate_kids_age])
     is_active         = models.BooleanField(default=True, null=False, blank=False)
-    demo_courses      = models.ManyToManyField(to=Course, related_name='kids_taken_demo', verbose_name="Demo Course Purchased")
+    demo_courses      = models.ManyToManyField(to=Course, related_name='kids_taken_demo', verbose_name="Demo Course Purchased", null=True, blank=True)
 
     history           = HistoricalRecords()
 
